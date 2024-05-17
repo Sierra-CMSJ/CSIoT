6c6f051fa77ee59180fa6b6f876adf6d9772cd32
Cristian Manuel Sierra Jerez <75026787+2192308@users.noreply.github.com>
2024-05-17 12:21:11 -0500

    Descifrado_ChaCha20.ino

--- ChaCha20_V1/Descifrado_ChaCha20/Descifrado_ChaCha20.ino
+++ ChaCha20_V1/Descifrado_ChaCha20/Descifrado_ChaCha20.ino
@@ -6,12 +6,12 @@
 ChaCha chacha;
 
 // Clave y vector de inicialización
-byte key256[32] = {0xFE, 0x04, 0xE5, 0x4B, 0x87, 0x21, 0x4F, 0x7C, 0x9B, 0x46, 0x7F, 0xF6, 0x19, 0x0D, 0xCA, 0x70,
-0xB7, 0xB9, 0x4C, 0x56, 0x28, 0x44, 0xE2, 0x1F, 0x88, 0x03, 0xF5, 0x5F, 0x97, 0xAA, 0x71, 0xBA}; // Esta clave será la misma con la que se cifró la información
+byte key256[32] = {0X9B, 0XA2, 0X1F, 0XC6, 0XB2, 0XCA, 0XF5, 0XDF, 0XFE, 0X43, 0X21, 0X60, 0X96, 0X56, 0XD3, 0X53, 0X25, 0X2D,
+ 0X7A, 0X9E, 0X4C, 0XCF, 0X11, 0XD2, 0X3A, 0X94, 0XF4, 0X89, 0X53, 0X82, 0X07, 0X83}; // Esta clave será la misma con la que se cifró la información
 byte iv[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 // Texto cifrado
-byte cipherText[] = {0x3F, 0x13, 0x04, 0xB0, 0x31, 0xEF, 0xD2, 0xB8, 0xB1, 0x65, 0xCC, 0x52, 0x11, 0x67, 0x1A, 0x3A};// Este texto cifrado será el recibido en la transmisión
+byte cipherText[] = {0XD6, 0X22, 0X77, 0XD8, 0XF4, 0X7B, 0X74, 0XC1, 0XDE, 0X1E, 0X65, 0X0D, 0X8A, 0XE7, 0XE8, 0XF2};// Este texto cifrado será el recibido en la transmisión
 
 
 
@@ -21,23 +21,14 @@ void decryptText(const byte *key, const byte *iv, const byte *ciphertext, size_t
     unsigned long elapsed;
     int count;
 
-    start = micros();
-    for (count = 0; count < 500; ++count) { // Hace 500 asignaciones de claves y vectores de inicio para medir el tiempo como un promedio
-    // Establece la clave y el vector de inicialización
-    chacha.setKey(key, 32);
-    chacha.setIV(iv, chacha.ivSize());
-    }
-    elapsed = micros() - start;
-    Serial.print(elapsed / 500.0); // Tiempo promedio que le tarda asignar las claves y el IV
-    Serial.print("us en asignación de clave e IV, ");
-    Serial.print((500.0 * 1000000.0) / elapsed);
-    Serial.println(" asignaciones por segundo");
-
     // Buffer para el texto desencriptado
     char* decryptedText = new char[size];
 
     start = micros();
     for (count = 0; count < 500; ++count) { // Descifra el texto 500 veces y con esto se espera obtener varias muestras de tiempo 
+    // Establece la clave y el vector de inicialización
+    chacha.setKey(key, 32);
+    chacha.setIV(iv, chacha.ivSize());
     // Desencripta el texto
     chacha.decrypt((byte *)decryptedText, ciphertext, size);
     }
@@ -47,6 +38,9 @@ void decryptText(const byte *key, const byte *iv, const byte *ciphertext, size_t
     Serial.print("us por byte descifrado, ");
     Serial.print((size * 500.0 * 1000000.0) / elapsed);
     Serial.println(" bytes descifrados por segundo");
+
+    // Evitamos caracteres de relleno
+    decryptedText[size] = '\0'; // Agrega el carácter nulo al final de la cadena esto detiene el proceso e imprime solo la información importante
 
     // Imprime el texto desencriptado en formato de cadena de texto
     Serial.print("Texto desencriptado: \"");
