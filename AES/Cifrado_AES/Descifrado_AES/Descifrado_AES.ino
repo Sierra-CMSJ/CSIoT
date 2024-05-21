97c07fd474f6c9e54f45151cdef93fec87d43bb6
Cristian Manuel Sierra Jerez <75026787+2192308@users.noreply.github.com>
2024-05-20 19:02:46 -0500

    Descifrado_AES.ino

--- AES/Cifrado_AES/Descifrado_AES/Descifrado_AES.ino
+++ AES/Cifrado_AES/Descifrado_AES/Descifrado_AES.ino
@@ -23,17 +23,34 @@ void hexStringToByteArray(const char* hexString, byte* byteArray, size_t length)
 
 void setup() {
   Serial.begin(115200);
+  unsigned long start;
+  unsigned long elapsed;
 
   // Convertir el texto cifrado a un array de bytes
   byte ciphertext[strlen(ciphertext_hex) / 2];
   hexStringToByteArray(ciphertext_hex, ciphertext, sizeof(ciphertext));
 
+  // Tamaño del texto
+  size_t size = strlen(ciphertext_hex);
+
   // Buffer para el texto descifrado
   char plaintext[sizeof(ciphertext) + 1];
 
+   // Medición del tiempo de cifrado
+  start=micros();
+  for(int count=0; count < 500; ++count){
+  // Reiniciar el IV a su valor original
+  byte aes_iv_reset[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00 };
+  memcpy(aes_iv, aes_iv_reset, sizeof(aes_iv));
   // Descifrar el texto
   aesLib.decrypt(ciphertext, sizeof(ciphertext), (byte*)plaintext, aes_key, sizeof(aes_key), aes_iv);
-
+  }
+  elapsed = micros() - start;
+  Serial.print(elapsed / (size * 500.0)); // Tiempo promedio que tarda en cifrar 
+  Serial.print("us por byte descifrado, ");
+  Serial.print((size * 500.0 * 1000000.0) / elapsed);
+  Serial.println(" bytes descifrados por segundo");
+  
   // Asegurarse de que el texto descifrado sea una cadena válida
   plaintext[sizeof(ciphertext)] = '\0';
 
