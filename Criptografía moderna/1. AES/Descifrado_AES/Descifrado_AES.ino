0c78ca9799305f8eeb842ff251f2f619e83ad055
Cristian Sierra <75026787+2192308@users.noreply.github.com>
2024-05-28 15:20:48 -0500

    Add files via upload

---
+++ Criptografía moderna/1. AES/Descifrado_AES/Descifrado_AES.ino
@@ -0,0 +1,63 @@
+#include <AESLib.h>
+
+AESLib aesLib;
+
+// Clave de cifrado AES
+byte aes_key[32] = { 0x6A, 0xBF, 0x2F, 0x4A, 0x41, 0xC5, 0xFF, 0x28, 0x32, 0x72, 0x51, 0x42, 0x3D, 0xE9, 0xF1, 0xD3, 0x44, 0x21, 0xCB,
+ 0x62, 0xD9, 0x8E, 0x88, 0x7F, 0x3C, 0x51, 0xE3, 0xFD, 0xC3, 0x4C, 0x70, 0x16 };
+
+// Vector de inicialización IV
+byte aes_iv[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00 };
+
+// Texto cifrado en formato hexadecimal
+const char* ciphertext_hex = "C611AB80A5D8DBF26F671759FD61630B";
+
+// Función para convertir una cadena hexadecimal a un array de bytes
+void hexStringToByteArray(const char* hexString, byte* byteArray, size_t length) {
+  for (size_t i = 0; i < length; ++i) {
+    unsigned int byte;
+    sscanf(hexString + 2 * i, "%02x", &byte);
+    byteArray[i] = byte;
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  unsigned long start;
+  unsigned long elapsed;
+
+  // Convertir el texto cifrado a un array de bytes
+  byte ciphertext[strlen(ciphertext_hex) / 2];
+  hexStringToByteArray(ciphertext_hex, ciphertext, sizeof(ciphertext));
+
+  // Tamaño del texto
+  size_t size = strlen(ciphertext_hex);
+
+  // Buffer para el texto descifrado
+  char plaintext[sizeof(ciphertext) + 1];
+
+   // Medición del tiempo de cifrado
+  start=micros();
+  for(int count=0; count < 500; ++count){
+  // Reiniciar el IV a su valor original
+  byte aes_iv_reset[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X00 };
+  memcpy(aes_iv, aes_iv_reset, sizeof(aes_iv));
+  // Descifrar el texto
+  aesLib.decrypt(ciphertext, sizeof(ciphertext), (byte*)plaintext, aes_key, sizeof(aes_key), aes_iv);
+  }
+  elapsed = micros() - start;
+  Serial.print(elapsed / (size * 500.0)); // Tiempo promedio que tarda en cifrar 
+  Serial.print("us por byte descifrado, ");
+  Serial.print((size * 500.0 * 1000000.0) / elapsed);
+  Serial.println(" bytes descifrados por segundo");
+  
+  // Asegurarse de que el texto descifrado sea una cadena válida
+  plaintext[sizeof(ciphertext)] = '\0';
+
+  // Imprimir el texto descifrado
+  Serial.print("Texto descifrado: ");
+  Serial.println(plaintext);
+}
+
+void loop() {
+}
---
+++ Criptografía moderna/4. HMAC-SHA-256/HMAC-SHA-256/HMAC-SHA-256.ino
@@ -0,0 +1,48 @@
+#include <Crypto.h>
+#include <SHA256.h>
+#include <string.h>
+
+#define HASH_SIZE 32
+#define BLOCK_SIZE 64
+
+SHA256 sha256;
+char frase[] = "APVCMSJ2024IOTTG"; // Define la frase como una variable global
+char clave[] = "miClaveSecreta";
+
+void printHash(uint8_t* hash) {
+    for (int i = 0; i < HASH_SIZE; i++) {
+        if (hash[i] < 16) {
+            Serial.print("0");
+        }
+        Serial.print(hash[i], HEX);
+    }
+    Serial.println();
+}
+
+void hmacData(const char* key, const char* data)
+{
+    uint8_t hmac[HASH_SIZE];
+    unsigned long start = micros();
+    for (int count = 0; count < 500; ++count) { // calcula el HMAC de la información 500 veces para saber tiempo promedio
+    sha256.resetHMAC(key, strlen(key));
+    sha256.update(data, strlen(data));
+    sha256.finalizeHMAC(key, strlen(key), hmac, sizeof(hmac));
+    }
+    unsigned long elapsed = micros() - start;
+
+    Serial.print(elapsed / (sizeof(frase) * 500.0)); // Tiempo promedio por byte hasheado
+    Serial.print("us por byte hasheado, ");
+    Serial.print((sizeof(frase) * 500.0 * 1000000.0) / elapsed);
+    Serial.println(" bytes hasheados por segundo");
+
+    printHash(hmac);
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    hmacData(clave, frase);
+}
+
+void loop(){
+}
